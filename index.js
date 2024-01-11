9ad9d7c00474fdd8450b3ba2078b293abfb2022a
Arvind <aruparekh@gmail.com>
2024-01-12 00:47:27 +0530

    feat: add bin field to package.json

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(`
+Hello there! 👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
+
+🚀 Currently, I'm exploring the tech universe, building cool projects and contributing to open source.
+🌟 Check out some of my work:
+   - GitHub: https://github.com/ArvindParekh
+   - Portfolio: https://arvindparekh-projects.netlify.app
+
+Let's connect and chat about coding, tech, or anything under the sun! 🌞
+
+Happy coding! 🚀
+`);
