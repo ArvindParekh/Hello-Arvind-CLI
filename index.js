4ff18aa2020c61a85f533475519785e12bd89daa
Arvind <aruparekh@gmail.com>
2024-01-12 19:47:23 +0530

    feat: update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 console.log(`
-Hello there! 👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
+Hello there! Thank you for npxing me 😎
+👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
 
 🚀 Currently, I'm exploring the tech universe, building cool projects and contributing to open source.
 🌟 Check out some of my work:
    - GitHub: https://github.com/ArvindParekh
-   - Portfolio: https://arvindparekh-projects.netlify.app
+   - Linktree: https://linktr.ee/arvindparekh
+   - Projects: https://arvindparekh-projects.netlify.app
 
 Let's connect and chat about coding, tech, or anything under the sun! 🌞
+DM me on Twitter here: https://twitter.com/ArvindParekh_21!
 
 Happy coding! 🚀
 `);
