9dc0c15bfc775492875d84d22ff4e0a7c95d803f
Arvind <aruparekh@gmail.com>
2024-01-14 00:11:14 +0530

    feat: new message structure

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
-console.log(`
-Hello there! Thank you for npxing me 😎
-👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
+// console.log(`
+// Hello there! Thank you for npxing me 😎
+// 👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
 
+// 🚀 Currently, I'm exploring the tech universe, building cool projects and contributing to open source.
+// 🌟 Check out some of my work:
+//    - GitHub: https://github.com/ArvindParekh
+//    - Linktree: https://linktr.ee/arvindparekh
+//    - Projects: https://arvindparekh-projects.netlify.app
+
+// Let's connect and chat about coding, tech, or anything under the sun! 🌞
+// DM me on Twitter here: https://twitter.com/ArvindParekh_21!
+
+// Happy coding! 🚀
+// `);
+
+console.log(`
+╔═════════════════════════════════╗
+║           Hello there           ║
+║     Thank you for npxing me!    ║                                 ║
+╚═════════════════════════════════╝
+
+👋 I'm Arvind, a passionate developer who loves turning coffee into code. ☕💻
 🚀 Currently, I'm exploring the tech universe, building cool projects and contributing to open source.
-🌟 Check out some of my work:
-   - GitHub: https://github.com/ArvindParekh
-   - Linktree: https://linktr.ee/arvindparekh
-   - Projects: https://arvindparekh-projects.netlify.app
+
+📚 Learn more about me:
+   GitHub: https://github.com/ArvindParekh
+   LinkedIn: https://www.linkedin.com/in/arvindparekh/
+   Projects: https://arvindparekh-projects.netlify.app
 
 Let's connect and chat about coding, tech, or anything under the sun! 🌞
 DM me on Twitter here: https://twitter.com/ArvindParekh_21!
